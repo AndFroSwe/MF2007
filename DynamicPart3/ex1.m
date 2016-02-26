3e2c4f80b8284bb59b31ed31cbfb12edcf44ac15
Spookz0r <gabriel.andersson@hotmail.com>
2016-02-26 16:03:17 +0100

    Task 3: Motor model finished

---
+++ DynamicPart3/ex1.m
@@ -0,0 +1,37 @@
+close all, clear all, clc
+
+%% Define parameters
+n = 5; %Gearbox ratio
+J1 = 1.8e-5; %Load inertia 1
+J2 = 1.8e-5; %Load inertia 2
+Vmax = 24; %Supply voltage
+Vmin = -24; %Supply voltage
+Imax = 430e-3; %Maximum current
+V_i = Vmax;
+V_w = Vmax;
+sim_v = Vmax;
+% Set up generic simulation parameters, change when using sine input or
+% karnop
+F_c = 1e-3;
+d_v = 1;
+enable_sin = 0;
+enable_karnop = 1;
+sim_sin_amp = 1;
+sim_sin_freq = 1;
+sim_quantization_interval = 0;
+sim_zoh_sample_time = 0;
+% Motor parameters
+motor_pars = struct(); % Create empty struct for motor
+motor_pars.R = 112; % Terminal resistance [Ohm]
+motor_pars.L = 11.4e-3;    % Terminal inductance [H]
+motor_pars.d = 3.8e-6; %Viscous friction in brushes
+motor_pars.k = 69.7e-3;    % Torque constant [Nm/A]
+motor_pars.J = 7.46e-7;  % Rotor inertia
+
+%% Velocity Control
+motor = motor_pars;
+J = J1+J2;
+% Model
+%   named motor_model.slx
+
+% P-control
