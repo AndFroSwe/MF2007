4391dc4d9475fb0aa9499efccd1acbe8cca5196b
AndFroSwe <gummianka88@gmail.com>
2016-02-28 12:46:59 +0100

    Corrected mistake in code, model should not contain inductance

--- DynamicPart3/ex1.m
+++ DynamicPart3/ex1.m
@@ -1,5 +1,5 @@
 close all, clear all, clc
-
+warning('off', 'all')
 %% Define parameters
 n = 5; %Gearbox ratio
 J1 = 1.8e-5; %Load inertia 1
@@ -28,7 +28,7 @@ motor_pars.d = 3.8e-6; %Viscous friction in brushes
 motor_pars.k = 69.7e-3;    % Torque constant [Nm/A]
 motor_pars.J = 7.46e-7;  % Rotor inertia
 
-%% Velocity Control
+%% Velocity Control, no inductance
 motor = motor_pars;
 J = J1+J2;
 Jtot = motor.J + J/n^2; % Total inertia including load inertia after gear
@@ -38,9 +38,9 @@ Jtot = motor.J + J/n^2; % Total inertia including load inertia after gear
 % Transfer function of system, without control
 s = tf('s');
 G_nom = motor.k;
-G_den = (s*Jtot + motor.d)*(s*motor.L + motor.R) + motor.k^2;
+G_den = (s*Jtot + motor.d)*(motor.R) + motor.k;
 G = G_nom/G_den;
-disp('Transfer function of motor with inductance and inertial load with viscous friction')
+disp('Transfer function of motor without inductance and inertial load with viscous friction')
 G
 
 % P-control
@@ -53,18 +53,49 @@ grid on
 % Run command "P = rlocfind(G) to find desired P value in plot
 % OBS
 % Closed loop system
-F = P;
-Gc = F*G/(1 + F*G);
+F_P= P;
+
+Gc_P = minreal(F_P*G/(1 + F_P*G));
 disp('Closed loop system')
-Gc
+Gc_P
 figure
-step(Gc);
+step(Gc_P);
 hold on
 grid on
-legend('Closed loop system', 'Open system')
 title(sprintf('Step response for closed loop system with K_p=%0.2f', P))
 
+% Get phase margin
+[Gm,Pm,Wgm,Wpm] = margin(Gc_P);
+fprintf('Phase margin is %0.3f\n', Pm)
 
+% Simulation parameters
+simtime = 10;
+enable_sin = 1;
+sim_sin_amp = 200;
+sim_sin_freq = 10;
+I = 0;
+D = 0;
+% Run simulation
+sim('motor.slx')
+figure
+plot(sim_reference)
+hold on 
+grid on
+plot(sim_output)
+title('Closed loop controller for DC motor, output velocity')
+legend('Reference', 'Ouput')
+xlabel('Time [s]')
+ylabel('\omega [rad/s]')
+
+%% Designing a PI controller
+% PI controller
+P = 20;
+I = 1;
+D = 0;
+F_PI = P + 1/s*I;
+Gc_PI = minreal(F_PI*G);
+disp('Transfer function of closed system with PI controller is')
+Gc_PI
 
 
 
