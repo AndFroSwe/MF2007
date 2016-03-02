01fce927dd63829ec0c90a7ab64e2c74fc3577c6
Spookz0r <gabriel.andersson@hotmail.com>
2016-03-02 09:50:40 +0100

    Finished Velocity DIscrete controller

--- DynamicPart3/ex1.m
+++ DynamicPart3/ex1.m
@@ -120,11 +120,26 @@ xlabel('Time [s]')
 ylabel('\omega [rad/s]')
 
 %% Discrete time controller
+% Set parameters
+enable_sin = 0;     % Set 1 to enable sine input
+zoh_sample_time = 0.002;   % Sample time in zoh [ms]
+pulses_per_rev = 1000;   % Pulses per revolution [-]
+sim_zoh = 0.02;    
+
+% Calculated parameters
+enable_karnop = 0;  % Dont use karnop
+sim_quantization_interval = 2*pi/pulses_per_rev;    % Ppr to degrees
+sim_zoh_sample_time = zoh_sample_time*1e-3;  % Sample time, from ms to s
+I = 2;
+G_d = c2d(G,0.002,'zoh');
+F_d = c2d(F_PI,0.002,'tustin');
+Gc_d =minreal( G_d*F_d/(1+G_d*F_d));
+
+figure
+pzmap(Gc_d)
 
 
-
-
-
+%% Position Control
 
 
 
