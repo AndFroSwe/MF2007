0a3b17fbed4fc31c6de90d04f40f7119713738c8
Adam <adamlang@kth.se>
2016-04-05 17:58:39 +0200

    Added all plots and completed 31

--- WorkshopB/Matlab/t3s.m
+++ WorkshopB/Matlab/t3s.m
@@ -77,9 +77,6 @@ R_vector = [1 0];
 F = tf(S_vector, R_vector);
 disp('Error feedback closed loop')
 Gc_error = minreal(F*Go/(1 + F*Go))
-figure
-pzmap(Gc_error)
-grid on
 title('PZ map for error feedback system')
 
 % Output feedback
@@ -93,11 +90,6 @@ T = tf(T_vector, [1]);
 R = tf(R_vector, [1]);
 S = tf(S_vector, [1]);
 Gc_output = minreal((B*T)/(A*R + B*S))
-figure
-pzmap(Gc_output)
-title('Output feedback pole zero map')
-grid on
-
 
 %% Sensitivity
 c = 0.5107;
@@ -108,14 +100,73 @@ Am = s+omega1;
 Ao = s+omega2;
 Go = B/A;
 
-Se = (A*R)/(Am*Ao)
-Te = (B*S)/(Am*Ao)
+Se = (A*R)/(Am*Ao);
+Te = (B*S)/(Am*Ao);
 
+figure(24)
 bode(Se)
 hold on
 bode(Te)
-
 title('Sensitivity functions')
-legend('Se', 'Te');
+
+
+figure(35)
+pzmap(Se, Te)
+
+%% 4: Controlling the motor
+%% 4.1: Velocity Controller
+Jtot = motor.J + J1/n^2;
+disp('Open loop system')
+
+% Transfer of motor without indctance
+Go = tf([motor.k/(motor.R*Jtot)], [1 (motor.d/Jtot + motor.k^2/(motor.R*Jtot))])
+
+% Parameters for Maple
+a = Go.num{1}(2)
+b = Go.den{1}(2)
+
+% Error feedback
+% Place poles
+omega1 = 10;
+omega2 = 50;
+
+% Calculate regulator parameters
+s1 = -(b-omega1-omega2)/a; % P 
+s2 = omega1*omega2/a; % I
+
+% Controller parameters, PI controller
+S_vector = [s1 s2];
+R_vector = [1 0];
+
+% Controller tf
+F = tf(S_vector, R_vector);
+disp('Error feedback closed loop')
+Gc_error = minreal(F*Go/(1 + F*Go))
+
+% Output feedback
+% Calculate T
+t0 = dcgain(tf(Go.num{1}, [1 omega1]))^-1;
+T_vector = t0*[1 omega2];
+
+A = tf(Go.den{1}, [1]);
+B = tf(Go.num{1}, [1]);
+T = tf(T_vector, [1]);
+R = tf(R_vector, [1]);
+S = tf(S_vector, [1]);
+Gc_output = minreal((B*T)/(A*R + B*S))
+
+s=tf('s');
+Am = s+omega1;
+Ao = s+omega2;
+Go = B/A;
+
+Se = (A*R)/(Am*Ao);
+Te = (B*S)/(Am*Ao);
+
+figure(35)
+hold on
+pzmap(Se,'g', Te, 'k')
+legend('Se', 'Te', 'quickSe', 'quickTe');
+
 
 
